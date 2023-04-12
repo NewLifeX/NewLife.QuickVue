15e2793e606697e508cbcecfd009e9f37b84e602
zk <zekunfang@163.com>
2023-04-12 14:26:36 +0800

    修复一些问题

---
+++ src/views/modules/admin/role.vue
@@ -0,0 +1,22 @@
+<template>
+  <Page @setting="setting"></Page>
+</template>
+
+<script setup lang="ts">
+import { ColumnKind } from '/@/api/page';
+import usePageSetting from '/@/hook/usePageSetting';
+
+const { setting } = usePageSetting({
+  columns: [
+    {
+      in: ColumnKind.ADD,
+      prop: 'sort',
+      component: 'select',
+      label: '33333',
+      props: {
+        options: [{id: 1, name: '1111'}]
+      }
+    }
+  ]
+})
+</script>
